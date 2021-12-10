faba650ec56f5dbb759a8be28f8033b72d2d4065
flyash <fenxuehaiyan@qq.com>
2021-12-10 17:09:40 +0800

    Remove inactive ad list

--- README.md
+++ README.md
@@ -32,7 +32,7 @@
 * iptables根据ipset转发指定流量
 * v2ray只负责进站出站
 
-需要用dnsmasq-full替换dnsmasq才能支持ipset，gw列表将子域名提升到了主域名，同时增加了一些常见的园外网站，加快访问速度。
+需要用dnsmasq-full替换dnsmasq才能支持ipset。
 
 该方案v2ray只负责部分流量，对于一般百兆宽带用户，基本可以满足日常使用，如使用xray的xtls协议性能还可进一步提升。
 
@@ -52,14 +52,11 @@
 
 gw模式不需要cn.ips文件。gw.hosts与gw-udp.hosts互斥，选择其一。
 
-ad增加了一个: ad-ext.hosts 为了避免较多的误伤，可以根据需要酌情使用。
+ad广告列表，可以根据需要决定是否使用。
 
 * [v2ray.service](./v2ray.service) # v2ray服务
 * [ad.hosts](./ad.hosts) # 屏蔽广告
-* [ad-ext.hosts](./ad-ext.hosts) # 更狠的屏蔽广告
-* [ad.ips](./ad.ips) # 广告ip
 * [gw.hosts](./gw.hosts) # 某个域名列表，用于gw模式
-* [gw.ips](./gw.ips) # 某个ip列表，用于gw模式
 * [gw-udp.hosts](./gw.hosts) # 某个域名列表，用于gw模式，通过UDP转发，默认使用8.8.8.8，可自行替换为其它
 * [cn.ips](./cn.ips) # 从apnic提取出来的ip段集合，用于cn模式（园内直连）
 
@@ -276,9 +273,7 @@ nginx需要对外提供https访问，相关教程很多，这里不再赘述。
 
 主要规则取自
 
-* https://github.com/h2y/Shadowrocket-ADBlock-Rules
-* https://github.com/neoFelhz/neohosts
-* https://github.com/Hackl0us/SS-Rule-Snippet
+* https://github.com/cokebar/gfwlist2dnsmasq
 * https://github.com/privacy-protection-tools/anti-AD
 
 生成的hosts文件不定期更新，你也可以clone到本地自己更新规则，添加删除你想要的site，或着fork一份做你想要的。
@@ -308,6 +303,9 @@ nginx需要对外提供https访问，相关教程很多，这里不再赘述。
   作战方针制定好了那就开始战略部署吧。早些年时候ss的解决方案正好可以参考，dnsmasq系列相关的教程多如牛毛。初版采用了dnsmasq+dnscrypt+ipset+iptables这一组合，使用一段时间后发现效果不好。由于提供dnscrypt解析的多为园外的服务器，解析速度不理想，很明显感觉网页打开缓慢，于是寻找新的方案。目前选择了dns-over-https这种，又名doh，具体是什么自行科普下。开始想自己搭建服务器，偶然发现红鱼已经有成熟的服务可用，尝试之后速度明显提升，不在卡白。openwrt安装也很简单，同样搜https_dns_proxy，个人觉得不用安装luci-app相关的，只要安装https_dns_proxy本身就可以了，luci那边界面配置没有自定义源，只有两个内置选项，用不起来。
 
 # 更新记录
+2021-12-10
+* 去掉停止更新的广告源
+
 2020-12-23
 * 添加目录
 * 整理文案
--- update.sh
+++ update.sh
@@ -12,62 +12,37 @@ wget -O apnic https://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest
 echo "create cn hash:net family inet hashsize 1024 maxelem 65536" > ../cn.ips
 cat apnic | awk -F\| '/CN\|ipv4/ { printf("add cn %s/%d\n", $4, 32-log($5)/log(2)) }' >> ../cn.ips
 
-wget -O sr.conf https://raw.githubusercontent.com/h2y/Shadowrocket-ADBlock-Rules/master/sr_top500_banlist_ad.conf
-
 # gw
-cat sr.conf | grep Proxy|grep DOMAIN-SUFFIX|awk -F, '{print $2}' > gw.tmp
-cat sr.conf | grep Proxy|grep IP-CIDR|awk -F, '{print $2}' > gw_ip
-echo "create gw hash:net family inet hashsize 1024 maxelem 65536" > ../gw.ips
-awk '{print "add gw "$0}' gw_ip >> ../gw.ips
+wget -O gw.tmp https://cokebar.github.io/gfwlist2dnsmasq/gfwlist_domain.txt
+
 # extend to top domain
 # cat gw | awk -F. '{if ($(NF-1) ~ /^(com|org|net|gov|edu|info|co|in)$/ && NF>2) print $(NF-2)"."$(NF-1)"."$(NF); else print $(NF-1)"."$(NF)}' > gw.tmp
-# Other popular sites
-wget -O other.conf https://raw.githubusercontent.com/Hackl0us/SS-Rule-Snippet/master/%E8%A7%84%E5%88%99%E7%89%87%E6%AE%B5%E9%9B%86/%E8%87%AA%E9%80%89%E8%A7%84%E5%88%99%E9%9B%86/%E5%B8%B8%E8%A7%81%E5%9B%BD%E5%A4%96%E7%BD%91%E7%AB%99%E5%88%97%E8%A1%A8.txt
-cat other.conf | grep Proxy|grep DOMAIN-SUFFIX|awk -F, '{print $2}' >> gw.tmp
+
 # Add custom domain
 cat ../config/gw.conf >> gw.tmp
 
 # Uniq and sort gw list
 sort -u -o gw gw.tmp
 
-# ad
-cat sr.conf | grep Reject|grep DOMAIN-SUFFIX|awk -F, '{print $2}' > ad
-# Another smaller ad hosts
-wget -O hosts https://cdn.jsdelivr.net/gh/neoFelhz/neohosts@gh-pages/basic/hosts
-sed -i.bak $'s/\r//g' hosts
-cat hosts | grep 0.0.0.0|awk '{print $2}' >> ad
-# Put anti-ad to ad-ext
+# Put anti-ad to ad
 wget -O anti https://raw.githubusercontent.com/privacy-protection-tools/anti-AD/master/adblock-for-dnsmasq.conf
-cat anti | grep address=/|awk -F/ '{print $2}' > ad-ext
+cat anti | grep address=/|awk -F/ '{print $2}' > ad
 # Add custom ad hosts
 cat ../config/ad.conf >> ad
 # Remove the first dot ex: .abc.com
 sed -i.bak 's/^\.//g' ad
 rm ad.bak
-# ad ips
-cat sr.conf | grep Reject|grep IP-CIDR|awk -F, '{print $2}' > ad_ip
-echo "create ad hash:net family inet hashsize 1024 maxelem 65536" > ../ad.ips
-awk '{print "add ad "$0}' ad_ip >> ../ad.ips
 
 # Uniq and sort ad list
 sort -u -o ad ad
-sort -u -o ad-ext ad-ext
-
-# Remove duplicate ad
-comm -2 -3 ad-ext ad > ad-ext.tmp
-rm ad-ext && mv ad-ext.tmp ad-ext
 
 # Allow ad in blank list
 comm -2 -3 ad ../config/ad_blank.conf > ad.tmp
-comm -2 -3 ad-ext ../config/ad_blank.conf > ad-ext.tmp
 rm ad && mv ad.tmp ad
-rm ad-ext && mv ad-ext.tmp ad-ext
 
 # Generate ad.hosts file for dnsmasq
 # awk '{print "0.0.0.0 "$0}' ad > ../ad.hosts
-# awk '{print "0.0.0.0 "$0}' ad-ext > ../ad-ext.hosts
 awk '{print "address=/"$0"/"}' ad > ../ad.hosts
-awk '{print "address=/"$0"/"}' ad-ext > ../ad-ext.hosts
 
 # Generate gw.hosts file for dnsmasq
 awk '{print "server=/"$0"/127.0.0.1#1053"}' gw > ../gw.hosts
